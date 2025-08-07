c22541b86652f56dfbeab4b443040ac756e97125
Your Name <you@example.com>
2025-08-07 11:53:38 +0600

    Added API

--- src/components/layouts/NewArrival.jsx
+++ src/components/layouts/NewArrival.jsx
@@ -15,64 +15,73 @@ import Bag from "../../assets/bag.png";
 import BagTwo from "../../assets/bagTwo.png";
 import HamalDista from "../../assets/hamal-dista.png";
 import Slider from "react-slick";
+import { useEffect, useState } from "react";
+import axios from "axios";
 // Images
 
 const NewArrival = () => {
+  //SLider
   var settings = {
-    dots: false,
+    dots: true,
     infinite: true,
     slidesToShow: 4,
-    slidesToScroll: 1,
+    slidesToScroll: 4,
     autoplay: true,
     pauseOnHover: true,
-    speed: 300,
-    autoplaySpeed: 2000,
-    cssEase: "linear"
+    speed: 700,
+    autoplaySpeed: 2500,
+    
   };
+  
+  //SLider
+
+  const [myProducts, setMyProducts] = useState([])
+
+  useEffect(() => {
+    async function x() {
+      let data = await axios.get('https://dummyjson.com/products')
+      setMyProducts(data.data.products)
+    }
+    x()
+  })
+
   return (
     <>
       <section>
         <Container className={"pb-20"}>
           <h3 className="font-bold text-[38px] pb-12">New Arrivals</h3>
-          <Slider {...settings}>
-            <Link to={"/shop"}>
-              <Product imgsrc={Clock} imgalt={"clock.png"} badgetxt={"10%"} />
-            </Link>
-            <Link to={"/shop"}>
-              <Product imgsrc={Watch} imgalt={"watch.png"} badgetxt={"New"} />
-            </Link>
-            <Link to={"/shop"}>
-              <Product imgsrc={Basket} imgalt={"basket.png"} badgetxt={"New"} />
-            </Link>
-            <Link to={"/shop"}>
-              <Product imgsrc={Stuff} imgalt={"stuff.png"} badgetxt={"New"} />
-            </Link>
-
-            <Link to={"/shop"}>
-              <Product imgsrc={Clock} imgalt={"clock.png"} badgetxt={"10%"} />
-            </Link>
+          <Slider  {...settings}>
+            {myProducts.map(items => (
+              <Link key={items.id} to={"/shop"} >
+                <Product
+                  imgsrc={items.thumbnail}
+                  imgalt={items.name}
+                  badgetxt={`${items.discountPercentage}%`}
+                  title={items.title}
+                  price={`$ ${items.price}`}
+                />
+              </Link>
+            ))}
           </Slider>
+
 
           <h3 className="font-bold pt-20 pb-12 text-[38px]">Our Bestsellers</h3>
-          <Slider className="flex" {...settings}>
-            <Link to={"/shop"}>
-              <Product imgsrc={Bottle} imgalt={"clock.png"} badgetxt={"New"} />
-            </Link>
-            <Link to={"/shop"}>
-              <Product imgsrc={Bag} imgalt={"watch.png"} badgetxt={"New"} />
-            </Link>
-            <Link to={"/shop"}>
-              <Product imgsrc={HamalDista} imgalt={"stuff.png"} badgetxt={"New"} />
-            </Link>
-            <Link to={"/shop"}>
-              <Product imgsrc={BagTwo} imgalt={"basket.png"} badgetxt={"New"} />{" "}
-            </Link>
-            <Link to={"/shop"}>
-              <Product imgsrc={Bottle} imgalt={"clock.png"} badgetxt={"New"} />
-            </Link>
+          <Slider  {...settings}>
+            {myProducts.map(items => (
+              <Link key={items.id} to={"/shop"} >
+                <Product
+                  imgsrc={items.thumbnail}
+                  imgalt={items.name}
+                  badgetxt={`${items.discountPercentage}%`}
+                  title={items.title}
+                  price={`$ ${items.price}`}
+                />
+              </Link>
+            ))}
           </Slider>
+          
         </Container>
-      </section>
+      </section >
     </>
   );
 };
--- src/components/layouts/Products.jsx
+++ src/components/layouts/Products.jsx
@@ -7,16 +7,16 @@ import { FaHeart, FaUndo } from "react-icons/fa";
 import { FaShoppingCart } from "react-icons/fa";
 // React Icons
 
-const Products = (props) => {
+const Products = (props ) => {
   return (
-    <div className="w-[305px] relative group duration-300">
+    <div className={`w-[305px] relative group duration-300 hover:scale-101`}>
       <Img src={props.imgsrc} alt={props.imgalt} />
       <span className="badge absolute bg-black text-white top-5 left-5 font-bold px-6 py-3.5 rounded-none">
         {props.badgetxt}
       </span>
       <div className="flex justify-between pt-5">
-        <p className="font-bold text-texProductColor">Basic Crew Neck Tee</p>
-        <p className="text-textColor font-xl">$44.00</p>
+        <p className="font-bold text-texProductColor">{props.title}</p>
+        <p className="text-textColor font-xl">{props.price}</p>
       </div>
       <div className="bg-white flex text-textColor p-5 gap-y-1 duration-300 flex-col items-end absolute bottom-10 w-full opacity-0 group-hover:opacity-100 transition-all ease-in-out  " >
         <p className="flex items-center text-[14px]  hover:text-black font-light">Add to Wish List <span className="text-black text-[14px] pl-2"><FaHeart /></span> </p>
